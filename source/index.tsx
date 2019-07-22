dd8a66b155056b02253d2905639b8fdd0cbc7a0e
Jordan Roher <jordanroher@MacBook-Pro.local>
2019-07-22 11:59:43 -0700

    First running build

---
+++ source/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { render } from "react-dom";
+
+
+render(
+	<p>This is an app</p>,
+	document.getElementById("app"),
+);
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const sourceFolder = "./source",
+	sourcePath = path.resolve(__dirname, sourceFolder),
+	destinationPath = path.resolve(__dirname, "game");
+
+module.exports = {
+    mode: "development",
+	context: path.resolve(__dirname),
+	entry: {
+		index: `${sourceFolder}/index.tsx`,
+	},
+	output: {
+		filename: "js/[name].js",
+		path: destinationPath,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			inject: true,
+			template: `${sourceFolder}/index.html`
+		}),
+	],
+	resolve: {
+		extensions: [".ts", ".tsx", ".js", ".jsx", ".png", ".jpg"],
+	},
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendors: {
+					test: /[\\/]node_modules[\\/]/,
+					name: "vendors",
+					enforce: true,
+					chunks: "all",
+				},
+			},
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				include: [sourcePath],
+				loader: ["ts-loader"],
+			}
+		],
+	},
+};
