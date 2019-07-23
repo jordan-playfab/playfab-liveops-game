4423974f9366e6ace928e6d103f24b4cd29bff83
Ashton Summers <asummers@microsoft.com>
2019-07-23 15:52:28 -0700

    Added title data key text box and state information.

--- source/app/pages/title-data.tsx
+++ source/app/pages/title-data.tsx
@@ -7,8 +7,25 @@ import { PrimaryButton } from 'office-ui-fabric-react';
 import { PlayFabHelper } from "../shared/playfab";
 
 type Props = IRouterProps;
+interface IState {
+    titleDataInput: string;
+    titleDataFilePath: string;
+}
 
-export default class TitleData extends React.Component<Props> {
+
+export default class TitleData extends React.Component<Props, IState> {
+
+
+    constructor() {
+        super(undefined);
+
+        this.state = {
+            titleDataInput: null,
+            titleDataFilePath: null
+        }
+
+        this.handleTitleDataInputEvent = this.handleTitleDataInputEvent.bind(this);
+    }
     public render(): React.ReactNode {
         if(is.null(this.props.titleID)) {
             return <Redirect to={routes.Home} />;
@@ -20,9 +37,24 @@ export default class TitleData extends React.Component<Props> {
                 <p>Your title ID is {this.props.titleID}</p>
                 <p>This page will help you load the required title data into your title.</p>
                 <p>This page hasn't been built yet.</p>
-                <p>Upload local title data: {this.renderUploadTitleDataButton()}</p>
+                <p>Upload local title data: {this.renderTitleDataKeyInput()} {this.renderUploadTitleDataButton()} </p>
             </React.Fragment>
         );
+    }
+
+    private renderTitleDataKeyInput() {
+        return (
+        <label>
+          Title data key:
+          <input type="text" value={this.state.titleDataInput} onChange={this.handleTitleDataInputEvent} />
+        </label>
+         );
+    }
+    
+
+    private handleTitleDataInputEvent(event: any) {
+        console.log("updated state to " + this.state.titleDataInput);
+        this.setState({titleDataInput: event.target.value});
     }
 
     private renderUploadTitleDataButton() : React.ReactNode {
@@ -32,13 +64,12 @@ export default class TitleData extends React.Component<Props> {
     }
 
     private uploadTitleData() {
-       // PlayFabHelper.uploadTitleData("Enemies", "testString", (data) => {
-          //  this.uploadSuccess(data);
-        //}, (error) =>{
-          //  alert("We failed to upload title data");
-        //})
-        // TODO read json file and add text field for title data to update
-        //PlayFabHelper.uploadTitleData()
+        alert("Title data input is " + this.state.titleDataInput);
+       PlayFabHelper.uploadTitleData(this.state.titleDataInput, "testString", (data) => {
+            this.uploadSuccess(data);
+        }, (error) =>{
+            alert("We failed to upload title data");
+        });
     }
 
     private uploadSuccess(result: PlayFabAdminModels.SetTitleDataResult){
