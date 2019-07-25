068c6cc0694b033e5cb9fc586410d1a57a867ccb
Jordan Roher <jordan@playfab.com>
2019-07-24 17:24:12 -0700

    Added footer

---
+++ source/app/components/footer.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import styled, { UlInline } from "../styles";
+
+const UlFooter = styled(UlInline)`
+    border-top: 2px solid ${s => s.theme.colorBorder200};
+    margin-top: 1em;
+    padding-top: 0.5em;
+    display: flex;
+    justify-content: space-evenly;
+`;
+
+const StrongTag = styled.strong`
+    display: block;
+`;
+
+export class Footer extends React.PureComponent<{}> {
+    public render(): React.ReactNode {
+        return (
+            <UlFooter>
+                <li>
+                    <StrongTag>Development</StrongTag>
+                    Jordan Roher
+                </li>
+                <li>
+                    <StrongTag>Scenario</StrongTag>
+                    Ashton Summers
+                </li>
+                <li>
+                    <StrongTag>Deployment</StrongTag>
+                    Julio Colon
+                </li>
+                <li>
+                    <StrongTag>Graphics</StrongTag>
+                    Aaron Lai
+                </li>
+            </UlFooter>
+        )
+    }
+}
