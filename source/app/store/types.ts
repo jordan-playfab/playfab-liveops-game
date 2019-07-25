5b9b232488c28c6f87763cbbac6e683d2d618605
Jordan Roher <jordanroher@MacBook-Pro.local>
2019-07-25 15:20:22 -0700

    Added Redux

---
+++ source/app/store/types.ts
@@ -0,0 +1,30 @@
+/// <reference path="../../../node_modules/playfab-web-sdk/src/Typings/PlayFab/PlayFabClientApi.d.ts" />
+/// <reference path="../../../node_modules/playfab-web-sdk/src/Typings/PlayFab/PlayFabAdminApi.d.ts" />
+import { IPlanetData, ITitleDataEnemies } from "../shared/types";
+
+export enum ActionTypes {
+    SET_TITLE_ID = "SET_TITLE_ID",
+    SET_PLAYER_ID = "SET_PLAYER_ID",
+    SET_PLAYER_NAME = "SET_PLAYER_NAME",
+    SET_CATALOG = "SET_CATALOG",
+    SET_INVENTORY = "SET_INVENTORY",
+    SET_STORES = "SET_STORES",
+    SET_PLANETS = "SET_PLANETS",
+    SET_ENEMIES = "SET_ENEMIES",
+}
+
+export interface IApplicationState {
+    titleId: string;
+    playerId: string;
+    playerName: string;
+    catalog: PlayFabClientModels.CatalogItem[];
+    inventory: PlayFabClientModels.GetUserInventoryResult;
+    stores: PlayFabClientModels.GetStoreItemsResult[];
+    planets: IPlanetData[];
+    enemies: ITitleDataEnemies;
+}
+
+export interface IAction<T> {
+    type: string;
+    payload?: T;
+}
