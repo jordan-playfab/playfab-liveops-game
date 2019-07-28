a9b8876121ac21576306964c8a3a75c9f44ae8cd
Jordan Roher <jordan@roher.me>
2019-07-28 15:04:13 -0700

    Improved login page flow

--- source/app/pages/login.tsx
+++ source/app/pages/login.tsx
@@ -1,45 +1,36 @@
 import React from "react";
+import { RouteComponentProps } from "react-router";
 import { TextField } from "office-ui-fabric-react/lib/TextField";
 import { PrimaryButton, MessageBar, MessageBarType, Spinner } from "office-ui-fabric-react";
+
 import { is } from "../shared/is";
 import { routes } from "../routes";
 import { PlayFabHelper } from "../shared/playfab";
-import { RouteComponentProps } from "react-router";
 import { Page } from "../components/page";
 import { DivConfirm } from "../styles";
 import { IWithAppStateProps, withAppState } from "../containers/with-app-state";
-import { actionSetPlayerId, actionSetPlayerName, actionSetCatalog, actionSetInventory, actionSetPlanetsFromTitleData, actionSetStoreNamesFromTitleData, actionSetPlayerHP, actionSetEnemiesFromTitleData, actionSetEquipmentMultiple, actionSetPlayerLevel, actionSetPlayerXP } from "../store/actions";
-import { TITLE_DATA_PLANETS, CATALOG_VERSION, TITLE_DATA_STORES, TITLE_DATA_ENEMIES, IStringDictionary } from "../shared/types";
+import { TITLE_DATA_PLANETS, CATALOG_VERSION, TITLE_DATA_STORES, TITLE_DATA_ENEMIES } from "../shared/types";
 import { IWithPageProps, withPage } from "../containers/with-page";
 import { IEquipItemInstance } from "../store/types";
 import { CloudScriptHelper } from "../shared/cloud-script";
+import { IPlayerLoginResponse } from "../../cloud-script/main";
+import { actionSetPlayerId, actionSetPlayerName, actionSetCatalog, actionSetInventory, actionSetPlanetsFromTitleData, actionSetStoreNamesFromTitleData, actionSetPlayerHP, actionSetEnemiesFromTitleData, actionSetEquipmentMultiple, actionSetPlayerLevel, actionSetPlayerXP } from "../store/actions";
 
 type Props = RouteComponentProps & IWithAppStateProps & IWithPageProps;
 
 interface IState {
     playerName: string;
     isLoggingIn: boolean;
-    equipment: IStringDictionary;
-    loadingCounter: number;
 }
 
 class LoginPageBase extends React.Component<Props, IState> {
-    private loadingMax = 3;
-
     constructor(props: Props) {
         super(props);
 
         this.state = {
             playerName: null,
             isLoggingIn: false,
-            equipment: null,
-            loadingCounter: 0,
         };
-    }
-
-    public componentDidUpdate(): void {
-        this.tryAndEquip();
-        this.redirectWhenDoneLoading();
     }
 
     public render(): React.ReactNode {
@@ -58,7 +49,7 @@ class LoginPageBase extends React.Component<Props, IState> {
                     <p>This login happens using <a href="https://api.playfab.com/documentation/client/method/LoginWithCustomID">Custom ID</a>.</p>
                     <fieldset>
                         <legend>Player</legend>
-                        <TextField label="Player ID" onChange={this.setLocalPlayerID} autoFocus />
+                        <TextField label="Player ID" onChange={this.onChangePlayerName} autoFocus />
                         <DivConfirm>
                             {this.state.isLoggingIn
                                 ? <Spinner label="Logging in" />
@@ -70,12 +61,6 @@ class LoginPageBase extends React.Component<Props, IState> {
         );
     }
 
-    private setLocalPlayerID = (_: any, newValue: string): void => {
-        this.setState({
-            playerName: newValue,
-        });
-    }
-
     private login = (): void => {
         this.props.onPageClearError();
 
@@ -83,72 +68,83 @@ class LoginPageBase extends React.Component<Props, IState> {
             isLoggingIn: true,
         });
 
-        PlayFabHelper.LoginWithCustomID(this.props.appState.titleId, this.state.playerName, (player) => {
-            this.props.dispatch(actionSetPlayerId(player.PlayFabId));
-            this.props.dispatch(actionSetPlayerName(this.state.playerName));
-
-            if(player.NewlyCreated) {
-                PlayFabHelper.UpdateUserTitleDisplayName(this.state.playerName, this.props.onPageNothing, this.props.onPageError);
-            }
-
-            CloudScriptHelper.login((response) => {
-                this.props.dispatch(actionSetPlayerHP(response.playerHP));
-                this.props.dispatch(actionSetPlayerLevel(response.level));
-                this.props.dispatch(actionSetPlayerXP(response.xp));
-                this.props.dispatch(actionSetInventory(response.inventory));
-
-                this.setState({
-                    equipment: response.equipment
-                }, this.advanceLoadCounter);
-            }, this.props.onPageError);
-
-            PlayFabHelper.GetTitleData([TITLE_DATA_PLANETS, TITLE_DATA_STORES, TITLE_DATA_ENEMIES], (data) => {
-                this.props.dispatch(actionSetPlanetsFromTitleData(data, TITLE_DATA_PLANETS));
-                this.props.dispatch(actionSetStoreNamesFromTitleData(data, TITLE_DATA_STORES));
-                this.props.dispatch(actionSetEnemiesFromTitleData(data, TITLE_DATA_ENEMIES));
-            }, this.props.onPageError);
-            
-            PlayFabHelper.GetCatalogItems(CATALOG_VERSION, (catalog) => {
-                this.props.dispatch(actionSetCatalog(catalog));
-            }, this.props.onPageError)
-            
-            this.advanceLoadCounter();
-        }, this.props.onPageError);
+        PlayFabHelper.LoginWithCustomID(this.props.appState.titleId, this.state.playerName, this.onLoginComplete, this.props.onPageError);
     }
 
-    private tryAndEquip(): void {
-        const shouldTryAndEquip = !is.null(this.props.appState.inventory) && !is.null(this.props.appState.inventory.Inventory) && !is.null(this.state.equipment);
+    private onLoginComplete = (player: PlayFabClientModels.LoginResult): void => {
+        this.props.dispatch(actionSetPlayerId(player.PlayFabId));
+        this.props.dispatch(actionSetPlayerName(this.state.playerName));
 
-        if(shouldTryAndEquip) {
-            const equipmentArray = Object.keys(this.state.equipment).map(slot => {
+        if(player.NewlyCreated) {
+            PlayFabHelper.UpdateUserTitleDisplayName(this.state.playerName, this.props.onPageNothing, this.props.onPageError);
+        }
+
+        CloudScriptHelper.login((response) => {
+            this.props.dispatch(actionSetPlayerHP(response.playerHP));
+            this.props.dispatch(actionSetPlayerLevel(response.level));
+            this.props.dispatch(actionSetPlayerXP(response.xp));
+            this.props.dispatch(actionSetInventory(response.inventory));
+            this.loadEquipment(response);
+        }, this.props.onPageError);
+
+        PlayFabHelper.GetTitleData([TITLE_DATA_PLANETS, TITLE_DATA_STORES, TITLE_DATA_ENEMIES], (data) => {
+            this.props.dispatch(actionSetPlanetsFromTitleData(data, TITLE_DATA_PLANETS));
+            this.props.dispatch(actionSetStoreNamesFromTitleData(data, TITLE_DATA_STORES));
+            this.props.dispatch(actionSetEnemiesFromTitleData(data, TITLE_DATA_ENEMIES));
+        }, this.props.onPageError);
+        
+        PlayFabHelper.GetCatalogItems(CATALOG_VERSION, (catalog) => {
+            this.props.dispatch(actionSetCatalog(catalog));
+        }, this.props.onPageError)
+        
+        this.goToGuide();
+    }
+
+    private loadEquipment(response: IPlayerLoginResponse): void {
+        if(is.null(response.equipment)) {
+            // You have no equipment
+            return;
+        }
+
+        const equipmentSlots = Object.keys(response.equipment);
+
+        if(is.null(equipmentSlots)) {
+            // You have an equipment log in user data, but nothing actually in there
+            return;
+        }
+
+        const equipment = equipmentSlots
+            .map(slot => {
+                const item = response.inventory.Inventory.find(i => i.ItemInstanceId === response.equipment[slot]);
+
+                if(is.null(item)) {
+                    // You have an item in your equipment list that isn't in your inventory. That's bad.
+                    // We'll filter these out
+                    return null;
+                }
+                
                 return {
                     slot,
-                    item: this.props.appState.inventory.Inventory.find(i => i.ItemInstanceId === this.state.equipment[slot])
+                    item,
                 } as IEquipItemInstance;
-            });
+            })
+            .filter(i => !is.null(i));
 
-            this.props.dispatch(actionSetEquipmentMultiple(equipmentArray));
-
-            this.setState({
-                equipment: null,
-            }, this.advanceLoadCounter);
-        }
+        this.props.dispatch(actionSetEquipmentMultiple(equipment));
     }
 
-    private redirectWhenDoneLoading(): void {
-        if(this.state.loadingCounter === this.loadingMax && this.props.appState.hasTitleId && this.props.appState.hasPlayerId) {
-            this.setState({
-                isLoggingIn: false,
-            }, () => {
-                this.props.history.push(routes.Guide(this.props.appState.titleId));
-            });
-        }
+    private goToGuide(): void {
+        this.setState({
+            isLoggingIn: false,
+        }, () => {
+            this.props.history.push(routes.Guide(this.props.appState.titleId));
+        });
     }
 
-    private advanceLoadCounter = (): void => {
-        this.setState(prevState => ({
-            loadingCounter: prevState.loadingCounter + 1,
-        }));
+    private onChangePlayerName = (_: any, playerName: string): void => {
+        this.setState({
+            playerName,
+        });
     }
 }
 
